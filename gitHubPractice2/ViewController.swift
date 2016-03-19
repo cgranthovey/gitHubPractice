39647d5a27481b6ee280971ae52ef8d20a1873be
Chris Hovey <chrishovey@chriss-air.byu.edu>
2016-03-19 16:32:53 -0600

    added UIView and constant

--- gitHubPractice2/ViewController.swift
+++ gitHubPractice2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let myName = "Christopher Hovey"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
